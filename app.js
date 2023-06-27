1ddf95281db662bf7944de7cc6a31f1f3b56fc89
Kruti Rathod <kruti@Krutis-MacBook-Air.local>
2023-06-27 13:33:14 -0700

    adding hello code

---
+++ app.js
@@ -0,0 +1,12 @@
+function randomColor(){
+    const r= Math.floor(Math.random()*256)
+    const g= Math.floor(Math.random()*256)
+    const b= Math.floor(Math.random()*256)
+    return `rgb(${r},${g},${b})`
+}
+const letters =document.querySelector('.letter')
+const clearInterval= setInterval(function(){
+    for(let letter of letters){
+        letter.style.color=randomColor()
+    }
+},1000)
